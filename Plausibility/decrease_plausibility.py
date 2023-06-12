56a270d2846c7ecef75f23ca50ecd2d2d967b0ac
Vatsal Raina <vatsalraina@hotmail.co.uk>
2023-06-12 23:23:22 +0100

    save gpt outputs in batches of 10

--- Plausibility/decrease_plausibility.py
+++ Plausibility/decrease_plausibility.py
@@ -4,8 +4,6 @@ import argparse
 import os
 import sys
 import json
-import numpy as np
-import random
 import openai
 
 parser = argparse.ArgumentParser(description='Get all command line arguments.')
@@ -21,18 +19,20 @@ def main(args):
         f.write(' '.join(sys.argv) + '\n')
         f.write('--------------------------------\n')
 
-    seed_val = 42
-    random.seed(seed_val)
-    np.random.seed(seed_val)
 
     openai.api_key = args.api_key
+
+    with open(args.save_path) as f:
+        expanded_examples = json.load(f)
+
+    start_point = len(expanded_examples)
 
     with open(args.subset_path) as f:
         sample_data = json.load(f)
 
-    expanded_examples = []
+    batch_examples = []
     for count, item in enumerate(sample_data):
-        if count == 50: break
+        if count < start_point: continue
         print(count)
         context, question, answer, distractors = item['context'], item['question'], item['answer'], item['distractors']
         distractors_string = ""
@@ -40,17 +40,20 @@ def main(args):
             distractors_string += dist + ' ; '
         distractors_string = distractors_string[:-3]
         prompt = "Consider a multiple-choice question with the following context:\n" + context + "\nThe question is:\n" + question + "\nThe correct answer is:\n" + answer + "\nThe distractor options are:\n" + distractors_string + "\nCan you make the distractors less plausible.\nPlease return only the new distractors with each one separated by a ; and no explanations."
-        print(prompt,'\n')
+        # print(prompt,'\n')
         model = "gpt-3.5-turbo"
         response = openai.ChatCompletion.create(model=model, messages=[{"role": "user", "content": prompt}])
         generated_text = response.choices[0].message.content.strip()
         distractors_more_plausible = generated_text.split(';')
         curr_example = {'context': context, 'question': question, 'answer': answer, 'distractors': distractors, 'distractors_more_plausible': distractors_more_plausible}
-        expanded_examples.append(curr_example)
-        
-
-    with open(args.save_path, 'w') as f:
-        json.dump(expanded_examples, f)
+        batch_examples.append(curr_example)
+        if len(batch_examples) == 10:
+            expanded_examples += batch_examples
+            batch_examples = []
+            with open(args.save_path, 'w') as f:
+                json.dump(expanded_examples, f)
+            print("Saved up to:", count)
+            print("----------------------")
 
 
 if __name__ == '__main__':

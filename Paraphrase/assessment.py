cffce54e856416b3ed0f0ae7cd9d0dd63b7d2057
Vatsal Raina <vatsalraina@hotmail.co.uk>
2023-07-18 09:39:30 +0100

    sentiment assessment

--- Paraphrase/assessment.py
+++ Paraphrase/assessment.py
@@ -4,8 +4,8 @@ import argparse
 import os
 import sys
 import json
-# from evaluate import load
-# from jiwer import wer
+from evaluate import load
+from jiwer import wer
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
@@ -90,19 +90,19 @@ def main(args):
     references = [ex['original'] for ex in examples]
     predictions = [ex['paraphrase'] for ex in examples]
 
-    sentiment_score = get_sentiment(predictions)
-    print('sentiment_mean : ', np.round(np.mean(sentiment_score)* 100, 1), 'sentiment_std : ', np.round(np.std(sentiment_score)* 100, 1))
+    # sentiment_score = get_sentiment(predictions)
+    # print('sentiment_mean : ', np.round(np.mean(sentiment_score)* 100, 1), 'sentiment_std : ', np.round(np.std(sentiment_score)* 100, 1))
 
-    # wer_score = get_wer(predictions, references)
-    # print('wer_mean : ', np.round(np.mean(wer_score)* 100, 1), 'wer_std : ', np.round(np.std(wer_score)* 100, 1))
+    wer_score = get_wer(predictions, references)
+    print('wer_mean : ', np.round(np.mean(wer_score)* 100, 1), 'wer_std : ', np.round(np.std(wer_score)* 100, 1))
 
-    # bert_score = get_bert_score(predictions, references)
-    # print('f1_mean : ', np.round(np.mean(bert_score['f1']) * 100, 1), 'f1_std : ', np.round(np.std(bert_score['f1']) * 100, 1))
-    # print('recall_mean : ',np.round( np.mean(bert_score['recall'])* 100, 1), 'recall_std : ', np.round(np.std(bert_score['recall'] )* 100, 1))
-    # print('precision_mean : ', np.round(np.mean(bert_score['precision'])* 100, 1), 'precision_std : ', np.round(np.std(bert_score['precision'])* 100, 1))
+    bert_score = get_bert_score(predictions, references)
+    print('f1_mean : ', np.round(np.mean(bert_score['f1']) * 100, 1), 'f1_std : ', np.round(np.std(bert_score['f1']) * 100, 1))
+    print('recall_mean : ',np.round( np.mean(bert_score['recall'])* 100, 1), 'recall_std : ', np.round(np.std(bert_score['recall'] )* 100, 1))
+    print('precision_mean : ', np.round(np.mean(bert_score['precision'])* 100, 1), 'precision_std : ', np.round(np.std(bert_score['precision'])* 100, 1))
 
-    # blue_score = get_blue_score(predictions, references)
-    # print('bleu score : ', blue_score)
+    blue_score = get_blue_score(predictions, references)
+    print('bleu score : ', blue_score)
 
     # ter_score = get_ter_score(predictions, references)
     # print('ter score : ', ter_score)
